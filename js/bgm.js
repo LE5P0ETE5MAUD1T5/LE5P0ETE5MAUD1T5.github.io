39f261ae81e0a382d79a04179279893da0b4dc6a
LE5P0ETE5MAUD1T5 <sjkths@gmail.com>
2025-12-23 19:40:24 +0900

    Update audio source paths in playlist

--- js/bgm.js
+++ js/bgm.js
@@ -2,11 +2,11 @@ const bgm = document.getElementById("bgm");
 
 /* 플레이리스트 */
 const playlist = [
-  {src: "../audio/bgm1.mp3", title: "하얀 달", artist: "Musical Rimbaud", thumb: "images/bgm-thumb1.jpg"},
-  {src: "../audio/bgm2.mp3", title: "오만의 목소리는 나팔의 거센 외침", artist: "Musical Rimbaud", thumb: "images/bgm-thumb2.jpg"},
-  {src: "../audio/bgm3.mp3", title: "니나의 대답", artist: "Musical Rimbaud", thumb: "images/bgm-thumb3.jpg"}
-  {src: "../audio/bgm4.mp3", title: "초록 Reprise", artist: "Musical Rimbaud", thumb: "images/bgm-thumb3.jpg"}
-  {src: "../audio/bgm5.mp3", title: "영원", artist: "Musical Rimbaud", thumb: "images/bgm-thumb3.jpg"}
+  {src: "/audio/bgm1.mp3", title: "하얀 달", artist: "Musical Rimbaud", thumb: "images/bgm-thumb1.jpg"},
+  {src: "/audio/bgm2.mp3", title: "오만의 목소리는 나팔의 거센 외침", artist: "Musical Rimbaud", thumb: "images/bgm-thumb1.jpg"},
+  {src: "/audio/bgm3.mp3", title: "니나의 대답", artist: "Musical Rimbaud", thumb: "images/bgm-thumb1.jpg"},
+  {src: "/audio/bgm4.mp3", title: "초록 Reprise", artist: "Musical Rimbaud", thumb: "images/bgm-thumb1.jpg"},
+  {src: "/audio/bgm5.mp3", title: "영원", artist: "Musical Rimbaud", thumb: "images/bgm-thumb1.jpg"},
   
 ];
 
